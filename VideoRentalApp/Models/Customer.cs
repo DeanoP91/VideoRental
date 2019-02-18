f7e9057563bac36ca546994fd614040548fd1e7b
Dean Potter <dean4365@gmail.com>
2019-02-18 23:37:24 +0000

    Populate Membership Types

--- VideoRentalApp/Models/Customer.cs
+++ VideoRentalApp/Models/Customer.cs
@@ -1,8 +1,13 @@
-﻿namespace VideoRentalApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoRentalApp.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        // Overriding the default conventions
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
